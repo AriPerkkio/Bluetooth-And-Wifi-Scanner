3680505232cff77cdd51df89454ab17c9ba61edf
AriPerkkio <t3pear00@students.oamk.fi>
2015-11-04 12:11:34 +0000

    getExtras updated, BtDevice started

--- BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/scanActivity.java
+++ BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/scanActivity.java
@@ -1,5 +1,10 @@
 package com.example.ariperkkio.btwifiscan;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -9,17 +14,29 @@ import android.widget.Toast;
 
 public class scanActivity extends Activity implements View.OnClickListener{
 
-    Button back;
+    private Button back;
+    private BluetoothAdapter btAdapter;
+    private BluetoothDevice btDevice;
 
-    String scanName;
-    int sampleRate;
+    private String scanName;
+    private int sampleRate;
 
     // booleans to check options for bluetooth
-    boolean btStatus;
-    boolean btDevName;
-    boolean btDevAddr;
-    boolean btDevType;
-    boolean btRSSI;
+    private boolean btStatus;
+    private boolean btDevName;
+    private boolean btDevAddr;
+    private boolean btDevType;
+    private boolean btRSSI;
+
+    private int btDeviceRSSI;
+
+    // booleans to check options for wifi
+    private boolean wifiStatus;
+    private boolean wifiSSID;
+    private boolean wifiBSSID;
+    private boolean wifiCapabilities;
+    private boolean wifiFrequency;
+    private boolean wifiRSSI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +55,36 @@ public class scanActivity extends Activity implements View.OnClickListener{
             btDevType = getIntent().getExtras().getBoolean("btDevType");
             btRSSI = getIntent().getExtras().getBoolean("btRSSI");
         }
-
+        // Get options for Wifi scanning
+        if(wifiStatus = getIntent().getExtras().getBoolean("wifiStatus")) {
+            wifiSSID = getIntent().getExtras().getBoolean("wifiSSID");
+            wifiBSSID = getIntent().getExtras().getBoolean("wifiBSSID");
+            wifiCapabilities = getIntent().getExtras().getBoolean("wifiCapabilities");
+            wifiFrequency = getIntent().getExtras().getBoolean("wifiFrequency");
+            wifiRSSI = getIntent().getExtras().getBoolean("wifiRSSI");
+        }
+        // Get scan name and sample rate
         scanName = getIntent().getExtras().getString("scanName");
         sampleRate = getIntent().getExtras().getInt("sampleRate");
 
-
     }
+
+    // BroadcastReceiver to receive data from btDiscrovery
+    final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                btDeviceRSSI = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
+                // device.getName()
+                // device.getAddress()
+                // btDeviceRSSI + "dBm"
+                // btDevice.getType()
+            }
+        }
+    };
 
     public void onClick(View v) {
         switch(v.getId()) {
