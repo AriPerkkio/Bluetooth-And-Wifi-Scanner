8f668738e2634476bdc05ad86ea1abdb4d7fa77d
AriPerkkio <t3pear00@students.oamk.fi>
2015-11-05 21:39:18 +0000

    Launching previousScan added

--- BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/MainActivity.java
+++ BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/MainActivity.java
@@ -31,15 +31,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick (View v) {
         switch(v.getId()) {
             case (R.id.mainBtnNewScan):
-                //do stuff
-                //Toast.makeText(this, "New scan", Toast.LENGTH_SHORT).show();
                 intent = new Intent(MainActivity.this, newScanActivity.class); //Create intent for newScanActivity
                 startActivity(intent);
             break;
 
             case (R.id.mainBtnHistory):
-                //do stuff
-                //Toast.makeText(this, "History", Toast.LENGTH_SHORT).show();
+                intent = new Intent(MainActivity.this, previousScansActivity.class); //Create intent for newScanActivity
+                startActivity(intent);
             break;
         }
     }
