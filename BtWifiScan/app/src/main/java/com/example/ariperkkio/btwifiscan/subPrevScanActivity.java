1a8d109bd488a498ac3fd6067a935e7ef9de22f1
AriPerkkio <t3pear00@students.oamk.fi>
2015-11-07 16:17:29 +0000

    Wifi and Bt results both working OK. CustomCursorAdapter, mergeCursor

--- BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/subPrevScanActivity.java
+++ BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/subPrevScanActivity.java
@@ -2,6 +2,7 @@ package com.example.ariperkkio.btwifiscan;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.MergeCursor;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -32,7 +33,9 @@ public class subPrevScanActivity extends Activity implements View.OnClickListene
     private ListView list;
     private databaseManager database;
     private subPrevScanCursorAdapter listAdapter;
-    private Cursor dataCursor;
+    private Cursor btCursor;
+    private Cursor wifiCursor;
+    private Cursor bothCursors;
 
 
     @Override
@@ -57,19 +60,21 @@ public class subPrevScanActivity extends Activity implements View.OnClickListene
         scanDate.setText(getIntent().getExtras().getString("scanDate"));
         scanId = getIntent().getExtras().getInt("scanId");
 
+        list = (ListView) findViewById(R.id.subPrevScanList);
+
         try{
             database.open();
-            dataCursor = database.getBtResultsById(scanId);
+            // BtResults and WifiResults have different columns - both need own cursors
+            btCursor = database.getBtResultsById(scanId);
+            wifiCursor = database.getWifiResultsById(scanId);
             database.close();
         }catch (SQLException e) {
             Log.e("getBtResultsById: ", e.toString());
         }
-
-        list = (ListView) findViewById(R.id.subPrevScanList);
-
-        listAdapter = new subPrevScanCursorAdapter(this, dataCursor, 0);
+        // MergeCursor combines both cursors into one
+        bothCursors = new MergeCursor(new Cursor[] {btCursor, wifiCursor});
+        listAdapter = new subPrevScanCursorAdapter(this, bothCursors, 0);
         list.setAdapter(listAdapter);
-
     }
 
     public void onClick(View v) {
@@ -79,22 +84,22 @@ public class subPrevScanActivity extends Activity implements View.OnClickListene
             break;
 
             case (R.id.subPrevScanRename):
+                // ToDo: sql update to specified row with new name
                 Toast.makeText(this, "rename", Toast.LENGTH_SHORT).show();
             break;
         }
     }
 
+    // Custom Cursor adapter to handle results from database
+    // Allows use of both wifi and bluetooth results - identified by columnCount
     private class subPrevScanCursorAdapter extends CursorAdapter {
-
         private LayoutInflater cursorInflater;
-
         public subPrevScanCursorAdapter (Context context, Cursor cursor, int flags) {
             super(context, cursor, flags);
             cursorInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
         public void bindView(View view, Context context, Cursor cursor) {
-
             ImageView icon = (ImageView) view.findViewById(R.id.scanrowIcon);
             TextView fieldOne = (TextView) view.findViewById(R.id.scanrowOne);
             TextView fieldTwo = (TextView) view.findViewById(R.id.scanrowTwo);
@@ -103,17 +108,37 @@ public class subPrevScanActivity extends Activity implements View.OnClickListene
             TextView fieldFive = (TextView) view.findViewById(R.id.scanrowFive);
             TextView fieldSix = (TextView) view.findViewById(R.id.scanrowSix);
 
+            // Bluetooth rows have 5 columns
+            if (cursor.getColumnCount() == 5) {
                 icon.setImageResource(R.drawable.bticon);
                 fieldOne.setText(cursor.getString(1));
                 fieldTwo.setText("");
                 fieldThree.setText(cursor.getString(2));
                 fieldFour.setText(cursor.getString(3));
-                fieldFive.setText("");
-                if(cursor.getString(4).equals("0")) // btRSSI is 0 when user unchecks it from newScanActivity
+                fieldFive.setText("Count: " + cursor.getColumnCount());
+                if (cursor.getString(4).equals("0")) // btRSSI is 0 when user unchecks it from newScanActivity
                     fieldSix.setText("");
                 else
-                    fieldSix.setText("RSSI: " + cursor.getString(4)+" dBm");
+                    fieldSix.setText("RSSI: " + cursor.getString(4) + " dBm");
             }
+
+            // Wifi results have 6 rows
+            if (cursor.getColumnCount()==6) {
+                icon.setImageResource(R.drawable.wifiicon);
+                fieldOne.setText(cursor.getString(1));
+                fieldTwo.setText(cursor.getString(2));
+                fieldThree.setText(cursor.getString(3));
+                fieldFour.setText("");
+                if(cursor.getString(4).equals("0")) // wifiFrequency is 0 when user unchecks it from newScanActivity
+                    fieldFive.setText("");
+                else
+                    fieldFive.setText(cursor.getString(4) + " MHz");
+                if(cursor.getString(5).equals("0")) // wifiRSSI is 0 when user unchecks it from newScanActivity
+                    fieldSix.setText("");
+                else
+                    fieldSix.setText("RSSI: "+cursor.getString(5)+" dbm"); // crashes when only int as text
+            }
+        }
 
         public View newView(Context context, Cursor cursor, ViewGroup parent) {
             return cursorInflater.inflate(R.layout.scanrow, parent, false);
