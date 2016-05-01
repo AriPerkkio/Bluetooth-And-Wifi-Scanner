03f243fb607c400159273a2d8e24d19085adec27
AriPerkkio <t3pear00@students.oamk.fi>
2016-05-01 20:47:24 +0100

    Buttons for global database

--- BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/MainActivity.java
+++ BtWifiScan/app/src/main/java/com/example/ariperkkio/btwifiscan/MainActivity.java
@@ -20,12 +20,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button buttonNew;
     private Button buttonHistory;
+    private Button buttonGlobal;
     private Intent intent;
     private databaseManager database;
     private TextView scanNumber;
     private TextView btNumber;
     private TextView wifiNumber;
     private final int REQ_FINE_LOCATION = 102;
+    private final int REQ_NETWORK_STATE = 103;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +38,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         buttonNew = (Button) findViewById(R.id.mainBtnNewScan);
         buttonHistory = (Button) findViewById(R.id.mainBtnHistory);
+        buttonGlobal = (Button) findViewById(R.id.mainBtnGlobal);
         buttonNew.setOnClickListener(this);
         buttonHistory.setOnClickListener(this);
+        buttonGlobal.setOnClickListener(this);
         scanNumber = (TextView) findViewById(R.id.mainScansNumber);
         btNumber = (TextView) findViewById(R.id.mainBtNumber);
         wifiNumber = (TextView) findViewById(R.id.mainWifiNumber);
@@ -60,6 +64,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQ_FINE_LOCATION);
             }
+            if (this.checkSelfPermission(Manifest.permission.INTERNET)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, REQ_NETWORK_STATE);
+            }
         }
     }
 
@@ -75,6 +83,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     Toast.makeText(this, "Please restart application and allow location access", Toast.LENGTH_SHORT).show();
                 }
             break;
+
+            case REQ_NETWORK_STATE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("Permission ", "Network access OK");
+                }
+                else {
+                    Toast.makeText(this, "Permission for network state is required for all features", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "Please restart application and allow network state access", Toast.LENGTH_SHORT).show();
+                }
+            break;
+
         }
     }
 
@@ -86,7 +105,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
             break;
 
             case (R.id.mainBtnHistory): // 'View History' button
-                intent = new Intent(MainActivity.this, previousScansActivity.class); //Create intent for newScanActivity
+                intent = new Intent(MainActivity.this, previousScansActivity.class);
+                startActivity(intent);
+            break;
+
+            case (R.id.mainBtnGlobal): // 'Global Database' button
+                intent = new Intent(MainActivity.this, GlobalActivity.class);
                 startActivity(intent);
             break;
         }
